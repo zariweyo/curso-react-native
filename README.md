c4105a02aad6e9bbda3d532e38951407d60ded03
daniel <dani@zariweyo.es>
2019-03-16 12:14:38 +0100

    cambios

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-## Renders 3
-* Flexbox
+## Componentes básicos del framework
+* Cuáles son los componentes básicos del framework
+* TextInput
+* Button
+* ScrollView
+* FlatList
 
 [Volver](https://github.com/zariweyo/curso-react-native)
--- src/Inicio.js
+++ src/Inicio.js
@@ -1,40 +1,90 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+    View, 
+    Text, 
+    StyleSheet,
+    TextInput,
+    Button,
+    ScrollView,
+    FlatList,
+    Image,
+    Picker
+} from 'react-native';
 import TextoVerde from './Componentes/TextoVerde';
+
+
 
 export default class Inicio extends Component{
 
     state = {
-        texto1:  "texto de cuadro 1",
-        texto2:  "texto de cuadro 2",
-        texto3:  "texto de cuadro 3",
+        listaDatos: [],
+        textoInput: ""
     }
     
 
     componentDidMount(){
+        var _listado = [];
+        for(var i=0;i<10;i++){
+            _listado.push(
+                {
+                    key:"D"+i,
+                    texto:'Dato '+i
+                }
+            );
+        }
         this.setState({
-            textoInicial: "texto ha cambiado"
+            listaDatos: _listado
         });
+    }
+
+    addLista(){
+        this.state.listaDatos.push({
+            key:"D"+Math.floor(Math.random()*1000000),
+            texto: this.state.textoInput
+        });
+
+        this.setState({
+            textoInput: "",
+            listaDatos: this.state.listaDatos
+        })
     }
 
     render(){
         return (
             <View style={styles.container}>
-                <View style={[styles.cuadro1,styles.cuadroBase]}>
-                    <TextoVerde
-                        texto={this.state.texto1}
-                    ></TextoVerde>
+                
+                <View style={styles.separador}>
+                    <ScrollView>
+                        {
+                            this.state.listaDatos.map(function(value,index){
+                                return (<TextoVerde key={index} texto={value.texto}></TextoVerde>) 
+                            }) 
+                        }
+                    </ScrollView>
+
                 </View>
-                <View style={[styles.cuadro2,styles.cuadroBase]}>
-                    <TextoVerde
-                        texto={this.state.texto2}
-                    ></TextoVerde>
+                <View style={styles.separador}>
+                    <TextInput
+                        style={styles.input}
+                        value={this.state.textoInput}
+                        onChangeText={(textoInput) => this.setState({textoInput})}
+                    ></TextInput>
+                    <TextoVerde texto={this.state.textoInput}></TextoVerde>
+                    <Button
+                        onPress={this.addLista.bind(this)}
+                        title="Añadir"
+                        style={styles.boton}
+                    ></Button>
                 </View>
-                <View style={[styles.cuadro3,styles.cuadroBase]}>
-                    <TextoVerde
-                        texto={this.state.texto3}
-                    ></TextoVerde>
+
+                <View style={styles.separador}>
+                    <FlatList
+                        data={this.state.listaDatos}
+                        extraData={this.state}
+                        renderItem={({item}) => <TextoVerde texto={item.texto}></TextoVerde>}
+                    />
                 </View>
+                
             </View>
         );
     }
@@ -42,25 +92,27 @@ export default class Inicio extends Component{
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      flexDirection:'row',
-      backgroundColor: '#F5FCFF',
+        flex: 1,
+        flexDirection:'column',
+        backgroundColor: '#F5FCFF',
     },
-    cuadroBase:{
+    separador:{
+        flex:1,
         justifyContent: 'center',
         alignItems: 'center',
+        borderWidth:2,
+        borderColor:"#228822"
     },
-    cuadro1:{
-        flex:0.2,
-        backgroundColor:"#114411"
+    input:{
+        borderWidth:1,
+        borderColor:"#000",
+        width:200,
+        borderRadius:10,
+        padding:2
     },
-    cuadro2:{
-        flex:0.4,
-        backgroundColor:"#441111"
-
-    },
-    cuadro3:{
-        flex:0.4,
-        backgroundColor:"#111144"
+    boton:{
+        backgroundColor:"#fff",
+        color:"#0888"
     }
 });
+
